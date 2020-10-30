b942482aef0047397ab80ba7f267648c0990d01b
RitikJaiswal75 <RitikJaiswal75@users.noreply.github.com>
2020-10-30 06:09:28 +0530

    Initial commit

---
+++ testings/requirements_Satisfier.py
@@ -0,0 +1,9 @@
+import os
+os.system('pip install pipwin')
+os.system('pipwin install pyaudio')
+os.system('pip install -Iv pyttsx3==2.6 -U')
+os.system('pip install speechRecognition')
+os.system('pip install --user --upgrade ezgmail')
+os.system('pip install wikipedia')
+os.system('pip install geocoder')
+os.system('pipwin install python-dateutil')
